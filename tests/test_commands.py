5fb0b3dbf2b23644f5dc2840ab6a6c4cc522320d
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2024-12-21 17:56:33 +0000

    Update: Modify url unit test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,12 +2,38 @@
 
 # type: ignore # pylint: disable=import-error
 
+import json
 import commands
 
-def test_http_uri():
+class TestUrlParser:
     """Test Regex URL Parsing"""
 
-    url = "https://github.com/0x15BA88FF/devault.git"
-    provider, directory, repository = commands.parse_url(url)
+    def test_http(self):
+        """Test http / https parsing"""
 
-    assert (provider, directory, repository) == ("github.com", "0x15ba88ff", "devault")
+        test_data_file = "./tests/test_files/test_http_urls.json"
+
+        with open(test_data_file, 'r', encoding="UTF-8") as file:
+            json_data = json.load(file)
+
+            for case in json_data:
+                provider, directory, repository = commands.parse_url(case["url"])
+
+                assert provider == case["provider"]
+                assert directory == case["directory"]
+                assert repository == case["repository"]
+
+    def test_ssh(self):
+        """Test ssh parsing"""
+
+        test_data_file = "./tests/test_files/test_ssh_urls.json"
+
+        with open(test_data_file, 'r', encoding="UTF-8") as file:
+            json_data = json.load(file)
+
+            for case in json_data:
+                provider, directory, repository = commands.parse_url(case["url"])
+
+                assert provider == case["provider"]
+                assert directory == case["directory"]
+                assert repository == case["repository"]
